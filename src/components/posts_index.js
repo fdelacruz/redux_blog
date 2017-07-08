fbb82497f7ed04ac24acade5abc035ea00c9ba7b
Francisco De La Cruz <helloquico@gmail.com>
2017-07-08 08:22:47 -0400

    Action Creator Shortcuts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
-class PostIndex extends Component {
+class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +16,6 @@ class PostIndex extends Component {
   }
 }
 
-export default PostIndex;
+// pass the action creator to connect inside of an object, { fetchPosts: fetchPosts }
+// instead of using the mapDisptchToProps function --and use ES6 to condense { fetchPosts }
+export default connect(null, { fetchPosts })(PostsIndex);
