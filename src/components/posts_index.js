700f22e70feb5e577cc590f3090fd54e2e29ff46
Francisco De La Cruz <helloquico@gmail.com>
2017-07-08 19:41:25 -0400

    Render a List of Posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -6,16 +7,33 @@ class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+  
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
+  }
 
   render() {
     return (
       <div>
-        Posts Index
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts }
+}
+
 // pass the action creator to connect inside of an object, { fetchPosts: fetchPosts }
 // instead of using the mapDisptchToProps function --and use ES6 to condense { fetchPosts }
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
