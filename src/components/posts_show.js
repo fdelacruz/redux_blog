f180482b66f3ff8f5f09ffd777d468d2dd96fd79
Francisco De La Cruz <helloquico@gmail.com>
2017-07-10 19:32:11 -0400

    Delete a Post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
@@ -11,27 +11,39 @@ class PostsShow extends Component {
     }
   }
 
-   render() {
-     const { post } = this.props;
+  onDeleteClick() {
+    const { id } = this.props.match.params;
 
-     if (!post) {
-       return <div>Loading...</div>
-     }
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
+  }
+  render() {
+    const { post } = this.props;
 
-     return (
-       <div>
-         <Link to="/">&lt; Back to Index</Link>
-         <h3>{post.title}</h3>
-         <h6>Categories: {post.categories}</h6>
-         <p>{post.content}</p>
+    if (!post) {
+      return <div>Loading...</div>
+    }
 
-       </div>
-     );
-   };
+    return (
+      <div>
+        <Link to="/">&lt; Back to Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right" 
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
+  };
 }
 
 function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] } 
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
